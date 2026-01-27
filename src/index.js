4fa1b192f933b86d389bec45d60bef27f9aa0b4e
Rajeshwar <rsr45411@gmail.com>
2026-01-27 17:41:02 +0530

    fixing cors

--- src/index.js
+++ src/index.js
@@ -11,13 +11,27 @@ import AnalyticsRoute from "./routes/analytics.route.js";
 
 Dotenv.config();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://your-netlify-site.netlify.app"
+];
 
 const app = express();
 app.use(express.json({limit:"16kb"}));
 app.use(urlencoded({extended:true,limit:'16kb'}));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173",credentials:true}));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // allow Postman / server-to-server
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use("/finPocket/api/auth/",AuthRoute);
 app.use("/finPocket/api/transaction",TransactionRoute);
